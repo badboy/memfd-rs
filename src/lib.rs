dfc0e6c4186b53fed3bf3d0d03d494ecda64eba7
Jan-Erik Rediger <janerik@fnordig.de>
2017-01-30 19:13:04 +0100

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//! # memfd - Convenience wrapper around `memfd_create(2)`
+//!
+//! Create an anonymous file
+//!
+//! The `memfd_create` creates an anonymous file and returns a file descriptor that refers to it.
+//! The file behaves like a regular file, and so can be modified, truncated, memory-mapped, and so
+//! on.
+//!
+//! For a detailed documentation see [the man page for `memfd_create(2)`](http://man7.org/linux/man-pages/man2/memfd_create.2.html).
+//!
+//! This library provides a small convenience wrapper around the syscall to open it as a regular file.
+//!
+//! ## Example
+//!
+//! ```
+//! use std::io::Write;
+//! let mut fd = memfd::create("anon_file").unwrap();
+//!
+//! fd.write_all(&b"Hello Rust!"[..]).unwrap();
+//! ```
+
 extern crate nix;
 
 use nix::sys::memfd::*;
@@ -8,11 +29,19 @@ use std::os::unix::io::FromRawFd;
 
 pub struct OpenOptions(MemFdCreateFlag);
 
+/// Options and flags which can be used to configure how a MemFd file is opened.
 impl OpenOptions {
+    /// Creates a blank new set of options ready for configuration.
+    ///
+    /// All options are initially set to `false`.
     pub fn new() -> OpenOptions {
         OpenOptions(MemFdCreateFlag::empty())
     }
 
+    /// Allow sealing operations on this file.
+    ///
+    /// See [`fcntl(2)`](http://man7.org/linux/man-pages/man2/fcntl.2.html) for available seal
+    /// operations.
     pub fn allow_sealing(&mut self, allow_sealing: bool) -> &mut OpenOptions {
         if allow_sealing {
             self.0.insert(MFD_ALLOW_SEALING)
@@ -22,6 +51,7 @@ impl OpenOptions {
         self
     }
 
+    /// Set the close-on-exec flag on the new file descriptor.
     pub fn close_on_exec(&mut self, cloexec: bool) -> &mut OpenOptions {
         if cloexec {
             self.0.insert(MFD_CLOEXEC)
